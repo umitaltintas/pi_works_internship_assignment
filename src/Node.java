712c3c299af23332b0367c5a354b84c8f73b7bed
umitaltintas <umitaltintas1@gmail.com>
2021-05-18 20:05:59 +0300

    feat: add insert and delete features to the tree

--- src/Node.java
+++ src/Node.java
@@ -1,71 +1,89 @@
 /**
- * Node
- * Generic node class for node used DS.
+ * Node Generic node class for node used DS.
  */
-public class  Node<T extends Comparable<T>> implements Comparable<Node<T>> {
-    Node<T> left;
-    Node<T> right;
-    T value;
+public class Node<T extends Comparable<T>> implements Comparable<Node<T>> {
+    private Node<T> left;
+    private Node<T> right;
+    private T value;
 
-    Node(T value){
-        this.value= value;
+    Node(T value) {
+        this.value = value;
     }
-    Node(){
+
+    Node() {
 
     }
-    boolean hasLeft(){
+
+    public boolean hasLeft() {
         return left != null;
     }
-    boolean hasRight(){
+
+    public boolean hasRight() {
         return right != null;
     }
-    boolean hasValue(){
+
+    public boolean hasValue() {
         return value != null;
     }
 
-    void setValue(T value){
+    public void setValue(T value) {
         this.value = value;
     }
-    T getValue(){
+
+    public T getValue() {
         return value;
     }
-    Node<T> getLeft(){
-        if(left == null ){
-            left=new Node<>();
+
+    public Node<T> getLeft() {
+        if (left == null) {
+            left = new Node<>();
         }
 
         return left;
     }
-    Node<T> getRight(){
-        if(right == null ){
-            right=new Node<>();
+
+    public Node<T> getRight() {
+        if (right == null) {
+            right = new Node<>();
         }
 
         return right;
     }
+
+    public void setLeft(Node<T> node) {
+        left = node;
+
+    }
+
+    public void setRight(Node<T> node) {
+
+        right = node;
+    }
+
     @Override
     public int compareTo(Node<T> o) {
         return this.value.compareTo(o.value);
     }
+
     @Override
     public boolean equals(Object o) {
-    
-         if (o == this) {
+
+        if (o == this) {
             return true;
         }
-  
-        
+
         if (!(o instanceof Node)) {
             return false;
         }
         Node<T> other = (Node<T>) o;
-        return this.value.compareTo(other.value)==0;
-        
+        return this.value.compareTo(other.value) == 0;
+
     }
+
     @Override
     public int hashCode() {
-    
+
         return value.hashCode();
     }
-    
+
 }
--- src/Tree.java
+++ src/Tree.java
@@ -1,18 +1,93 @@
-public class Tree <T extends Comparable<T>> {
-    
-    Node<T> rootNode;
+public class Tree<T extends Comparable<T>> {
+
+    private Node<T> rootNode;
 
     public Tree() {
         rootNode = new Node<>();
     }
 
-    public void  insert (T value) {
+    public void insert(T value) {
         Node<T> cursorNode = rootNode;
         while ((cursorNode.hasValue())) {
-            if(cursorNode.getValue().compareTo(value)>0)
-            {
-                cursorNode=cursorNode.getLeft();
+            if (cursorNode.getValue().compareTo(value) > 0) {
+                cursorNode = cursorNode.getLeft();
+            } else {
+                cursorNode = cursorNode.getRight();
             }
         }
+        cursorNode.setValue(value);
+    }
+
+    public void delete(T value) {
+        Node<T> sNode = new Node<>(value);
+
+        Node<T> parent = null;
+
+        Node<T> curr = rootNode;
+
+        while (curr != null && !curr.equals(sNode)) {
+
+            parent = curr;
+
+            if (0 < curr.compareTo(sNode)) {
+                curr = curr.getLeft();
+            } else {
+                curr = curr.getRight();
+            }
+        }
+        if (curr == null) {
+            return;
+        }
+
+        if (!curr.hasLeft() && !curr.hasRight()) {
+
+            if (curr != rootNode) {
+                if (parent.getLeft().equals(curr)) {
+                    parent.setLeft(null);
+                } else {
+                    parent.setRight(null);
+                }
+            }
+
+            else {
+                rootNode = null;
+            }
+        }
+
+        else if (curr.hasLeft() && curr.hasRight()) {
+
+            Node<T> successor = getMinimumKey(curr.getRight());
+
+            T val = successor.getValue();
+
+            delete(successor.getValue());
+
+            curr.setValue(val);
+        }
+
+        else {
+
+            Node<T> child = (curr.hasLeft()) ? curr.getLeft() : curr.getRight();
+
+            if (curr.equals(rootNode)) {
+                if (curr.equals(parent.getLeft())) {
+                    parent.setLeft(child);
+                } else {
+                    parent.setRight(child);
+                }
+            }
+
+            else {
+                rootNode = child;
+            }
+        }
+
+    }
+
+    public Node<T> getMinimumKey(Node<T> curr) {
+        while (curr.hasLeft()) {
+            curr = curr.getLeft();
+        }
+        return curr;
     }
 }
